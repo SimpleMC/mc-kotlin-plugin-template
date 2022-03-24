8fea6bc6b37f5ae0db3d2022a2c649c14bfe9359
Taylor Becker <taylor.becker@dotrun.io>
2022-03-24 18:48:41 -0400

    Update dependencies to latest and improve plugin artifacts (#6)
    * Update dependencies to latest and improve plugin artifacts
    
    Update java, kotlin, gradle, spigot, ktlint, etc
    Switch to incrementMinorIfNotOnRelease version incrementer
    Improved packaging/artifacts by providing a plugin jar that will load libs (via spigot's `libraries` in plugin.yml) and an "offline" shadowed/fat jar
    shadowJar dependency relocation to prevent classpath pollution
    Simplify automated CHANGELOG update by making the format predictable
    
    * \N sequence was not working for changelog regex, replace with negated \n
    
    * Use default 'v' tag prefix instead of 'release-'
    
    * Fix first tag link
    
    * Release version: 0.1.0
    
    * Revert "Release version: 0.1.0" as it was just a test
    
    This reverts commit 919b163f1dc009ad4c7cb54e496ced292b201f05.
    
    * Add comment explaining the plugin.yml offline copy
    
    * Replace jcenter with mavenCentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,85 +1,72 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
 import pl.allegro.tech.build.axion.release.domain.hooks.HookContext
 import pl.allegro.tech.build.axion.release.domain.hooks.HooksConfig
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
-plugins {
-    kotlin("jvm") version "1.3.61"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("pl.allegro.tech.build.axion-release") version "1.10.3"
-    id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.yaml:snakeyaml:1.30")
+    }
 }
 
-val repoRef = "SimpleMC\\/mc-kotlin-plugin-template"
-val mcApiVersion = "1.15"
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("pl.allegro.tech.build.axion-release") version "1.13.6"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
 
 group = "org.simplemc"
 version = scmVersion.version
 
+val mcApiVersion: String by project
+val repoRef: String by project
+
 scmVersion {
-    hooks(closureOf<HooksConfig> {
-        pre(
-            "fileUpdate",
-            mapOf(
-                "file" to "src/main/resources/plugin.yml",
-                "pattern" to KotlinClosure2<String, HookContext, String>({ v, _ -> "version: $v\\napi-version: \".+\"" }),
-                "replacement" to KotlinClosure2<String, HookContext, String>({ v, _ -> "version: $v\napi-version: \"$mcApiVersion\"" })
+    versionIncrementer("incrementMinorIfNotOnRelease", mapOf("releaseBranchPattern" to "release/.+"))
+
+    hooks(
+        closureOf<HooksConfig> {
+            pre(
+                "fileUpdate",
+                mapOf(
+                    "file" to "CHANGELOG.md",
+                    "pattern" to KotlinClosure2<String, HookContext, String>({ v, _ ->
+                        "\\[Unreleased\\]([\\s\\S]+?)\\n(?:^\\[Unreleased\\]: https:\\/\\/github\\.com\\/$repoRef\\/compare\\/[^\\n]*\$([\\s\\S]*))?\\z"
+                    }),
+                    "replacement" to KotlinClosure2<String, HookContext, String>({ v, c ->
+                        """
+                            \[Unreleased\]
+                            
+                            ## \[$v\] - ${currentDateString()}$1
+                            \[Unreleased\]: https:\/\/github\.com\/$repoRef\/compare\/v$v...HEAD
+                            \[$v\]: https:\/\/github\.com\/$repoRef\/${if (c.previousVersion == v) "releases/tag/v$v" else "compare/v${c.previousVersion}...v$v"}${'$'}2
+                        """.trimIndent()
+                    })
+                )
             )
-        )
-        // "normal" changelog update--changelog already contains a history
-        pre(
-            "fileUpdate",
-            mapOf(
-                "file" to "CHANGELOG.md",
-                "pattern" to KotlinClosure2<String, HookContext, String>({ v, _ ->
-                    "\\[Unreleased\\]([\\s\\S]+?)\\n(?:^\\[Unreleased\\]: https:\\/\\/github\\.com\\/$repoRef\\/compare\\/release-$v\\.\\.\\.HEAD\$([\\s\\S]*))?\\z"
-                }),
-                "replacement" to KotlinClosure2<String, HookContext, String>({ v, c ->
-                    """
-                        \[Unreleased\]
-                        
-                        ## \[$v\] - ${currentDateString()}$1
-                        \[Unreleased\]: https:\/\/github\.com\/$repoRef\/compare\/release-$v...HEAD
-                        \[$v\]: https:\/\/github\.com\/$repoRef\/compare\/release-${c.previousVersion}...release-$v$2
-                    """.trimIndent()
-                })
-            )
-        )
-        // first-time changelog update--changelog has only unreleased info
-        pre(
-            "fileUpdate",
-            mapOf(
-                "file" to "CHANGELOG.md",
-                "pattern" to KotlinClosure2<String, HookContext, String>({ v, _ ->
-                    "Unreleased([\\s\\S]+?\\nand this project adheres to \\[Semantic Versioning\\]\\(https:\\/\\/semver\\.org\\/spec\\/v2\\.0\\.0\\.html\\).)\\s\\z"
-                }),
-                "replacement" to KotlinClosure2<String, HookContext, String>({ v, c ->
-                    """
-                        \[Unreleased\]
-                        
-                        ## \[$v\] - ${currentDateString()}$1
-                        
-                        \[Unreleased\]: https:\/\/github\.com\/$repoRef\/compare\/release-$v...HEAD
-                        \[$v\]: https:\/\/github\.com\/$repoRef\/releases\/tag\/release-$v
-                    """.trimIndent()
-                })
-            )
-        )
-        pre("commit")
-    })
+            pre("commit")
+        }
+    )
 }
 
 fun currentDateString() = OffsetDateTime.now(ZoneOffset.UTC).toLocalDate().format(DateTimeFormatter.ISO_DATE)
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
@@ -89,42 +76,57 @@ dependencies {
     compileOnly(group = "org.spigotmc", name = "spigot-api", version = "$mcApiVersion+")
 }
 
-ktlint {
-    // FIXME - ktlint bug(?): https://github.com/pinterest/ktlint/issues/527
-    disabledRules.set(listOf("import-ordering"))
-}
-
 tasks {
     wrapper {
-        gradleVersion = "6.1.1"
+        gradleVersion = "7.4.1"
         distributionType = Wrapper.DistributionType.ALL
     }
 
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = "1.8"
+    processResources {
+        val placeholders = mapOf(
+            "version" to version,
+            "apiVersion" to mcApiVersion,
+            "kotlinVersion" to project.properties["kotlinVersion"]
+        )
+
+        filesMatching("plugin.yml") {
+            expand(placeholders)
+        }
+
+        // create an "offline" copy/variant of the plugin.yml with `libraries` omitted
+        doLast {
+            val resourcesDir = sourceSets.main.get().output.resourcesDir
+            val yamlDumpOptions =
+                // make it pretty for the people
+                DumperOptions().also {
+                    it.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
+                    it.setPrettyFlow(true)
+                }
+            val yaml = Yaml(yamlDumpOptions)
+            val pluginYml: Map<String, Any> = yaml.load(file("$resourcesDir/plugin.yml").inputStream())
+            yaml.dump(pluginYml.filterKeys { it != "libraries" }, file("$resourcesDir/offline-plugin.yml").writer())
         }
     }
 
-    // standard jar should be ready to go with all dependencies
+    jar {
+        exclude("offline-plugin.yml")
+    }
+
+    // offline jar should be ready to go with all dependencies
     shadowJar {
         minimize()
-        archiveClassifier.set("")
+        archiveClassifier.set("offline")
+        exclude("plugin.yml")
+        rename("offline-plugin.yml", "plugin.yml")
     }
 
-    // nokt jar without the kotlin runtime
-    register<ShadowJar>("nokt") {
-        minimize()
-        archiveClassifier.set("nokt")
-        from(sourceSets.main.get().output)
-        configurations = listOf(project.configurations.runtimeClasspath.get())
-
-        dependencies {
-            exclude(dependency("org.jetbrains.*:"))
-        }
+    // avoid classpath conflicts/pollution via relocation
+    val configureShadowRelocation by registering(ConfigureShadowRelocation::class) {
+        target = shadowJar.get()
+        prefix = "${project.group}.${project.name.toLowerCase()}.libraries"
     }
 
     build {
-        dependsOn(":shadowJar", ":nokt")
+        dependsOn(shadowJar).dependsOn(configureShadowRelocation)
     }
 }
